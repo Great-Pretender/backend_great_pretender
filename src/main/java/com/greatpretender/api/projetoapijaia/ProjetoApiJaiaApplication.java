b9a212992e8c95dca6cb1253250ba47cfbb62790
guilherme0066 <cursioalves@gmail.com>
2023-09-20 22:41:05 -0300

    Implementando banco de dados

---
+++ src/main/java/com/greatpretender/api/projetoapijaia/ProjetoApiJaiaApplication.java
@@ -0,0 +1,13 @@
+package com.greatpretender.api.projetoapijaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiJaiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiJaiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greatpretender/api/projetoapijaia/ProjetoApiJaiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatpretender.api.projetoapijaia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiJaiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
